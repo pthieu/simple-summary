dd46854ba8cb58c0616daca71e44ef6cc058ab6f
Phong Thieu <pthieu@gmail.com>
2016-10-24 17:09:18 -0400

    package: initial push, working mvp

---
+++ examples/summarize-blog.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var fs = require('fs');
+
+var path = require('path');
+var summarize = require('../index');
+
+var text = fs.readFileSync(path.resolve(__dirname, '..', 'data',
+  'blog-example.txt'), 'utf-8');
+
+var summary = summarize(text);
+console.log(summary);
+console.log('FULL TEXT LENGTH: %s char', text.length);
+console.log('SUMMARY LENGTH: %s char', summary.length);
+var sigDig = 2;
+var compression = Math.round(
+  (text.length - summary.length) / text.length * Math.pow(10, sigDig + 2)
+) / Math.pow(10, sigDig);
+console.log('Compression: %d%', compression);
